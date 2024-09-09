e98bee3735480f19b76ea2c595c66816b377c86a
Kgaladi Sekgoka <129772660+KgaladiTumisoSekgoka@users.noreply.github.com>
2024-09-09 13:23:05 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace POEPart1
+{
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "server=localhost;database=cmcs;uid=root;";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }    
+    }
+}
